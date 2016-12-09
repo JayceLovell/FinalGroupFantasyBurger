8f47704f82eea79ee5b36b1fc1fcf16c600a954c
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-12-09 17:04:14 -0500

    Added All Shopping Cart Models

---
+++ FantasyBurgers/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FantasyBurgers.Models;
+
+namespace FantasyBurgers.ViewModels
+{
+    public class ShoppingCartRemoveViewModel
+    {
+        public string Message { get; set; }
+
+        public decimal CartTotal { get; set; }
+
+        public int CartCount { get; set; }
+
+        public int ItemCount { get; set; }
+
+        public int DeleteId { get; set; }
+    }
+}
---
+++ FantasyBurgers/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FantasyBurgers.Models;
+
+namespace FantasyBurgers.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
