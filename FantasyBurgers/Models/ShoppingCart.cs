3a0a7179f9fcb7e143813df5ce783319b73144d3
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-12-09 18:22:01 -0500

    Re comiiting

--- FantasyBurgers/Models/ShoppingCart.cs
+++ FantasyBurgers/Models/ShoppingCart.cs
@@ -49,7 +49,6 @@ namespace FantasyBurgers.Models
             var cartItem = storeDB.Carts.SingleOrDefault(
 
                 c => c.CartId == ShoppingCartId
-                // PROBLEM IS AT LINE BELOW
                 && c.AppetizersId == appetizer.AppetizerId);
 
 
